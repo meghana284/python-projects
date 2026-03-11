f24d8d998861d8c0d2eb971857f1e6c1a31f53d7
meghana284 <aluvalameghana6204@gmail.com>
2026-03-11 19:28:33 +0530

    Added number guessing game project

---
+++ output.py
@@ -0,0 +1,15 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/megha/OneDrive/Desktop/Python Basic Projects/Number Guessing Game/game.pu.py
+Number Guessing Game
+Guess a number between 1 and 100
+Enter your guess: 50
+Too high! Try again
+Enter your guess: 25
+Too low! Try again
+Enter your guess: 40
+Too high! Try again
+Enter your guess: 30
+Congratulations! You guessed the correct number
+>>> 
