254a9661d0e65603b69c134dbed4e341c6949793
meghana284 <aluvalameghana6204@gmail.com>
2026-03-13 21:34:27 +0530

    Added contact book project

---
+++ contact output.py
@@ -0,0 +1,42 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/megha/OneDrive/Desktop/Python Basic Projects/Contact Book/Contact.py
+
+Contact Book
+1. Add Contact
+2. View Contacts
+3. Search Contact
+4. Exit
+Enter your choice: 1
+Enter name: meghana
+Enter phone number: 9876543210
+Contact added
+
+Contact Book
+1. Add Contact
+2. View Contacts
+3. Search Contact
+4. Exit
+Enter your choice: 2
+
+Contact List:
+meghana : 9876543210
+
+Contact Book
+1. Add Contact
+2. View Contacts
+3. Search Contact
+4. Exit
+Enter your choice: 3
+Enter name to search: meghana
+meghana : 9876543210
+
+Contact Book
+1. Add Contact
+2. View Contacts
+3. Search Contact
+4. Exit
+Enter your choice: 4
+Exiting...
+>>> 
