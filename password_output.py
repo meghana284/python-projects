55fc363df12c614cbf8a1e97aa8fe84d15751583
meghana284 <aluvalameghana6204@gmail.com>
2026-03-14 17:29:54 +0530

    Added password generator project

---
+++ password_output.py
@@ -0,0 +1,8 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/megha/OneDrive/Desktop/Python Basic Projects/password generator/password generator.py
+Password Generator
+Enter password length: 8
+Generated Password: xYaD:"Fy
+>>> 
